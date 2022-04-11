3a656d5f29f06f0a39dbdfffbb4e7e51c63420f8
pradeep dheke <pradeepbarca029@gmail.com>
2022-04-11 15:08:37 +1000

    creating versions of api

---
+++ README.md
@@ -0,0 +1,6 @@
+# Not to do api
+This api is granted for not to do application.
+Here is the link to front end app ....
+
+## APIS
+All the api will be following the `{rooturl}/api/v1` format.
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const express = require('express:');
+// const express = require('express');
+import express from 'express'
 const app = express();
 const PORT = 8000;
 
+// API end points
+
+app.get("/api/v1/", (req, res) => {
+    res.json({
+        message: "Welcome to the api"
+    })
+
+})
+
+
+app.use("/", (req, res) => {
+    // const person = {
+    //     name: "Pradeep",
+    //     lastName: "Dheke"
+    // }
+    res.json({message: "You have reached to the api of not-to-do application"});
+})
 
 app.listen(PORT, error => {
     error
